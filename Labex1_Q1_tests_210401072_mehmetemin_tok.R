fabbe03158a4e7ca1f955a20219973d194c3662e
Mehmet Emin Tok <mhmtmntok@gmail.com>
2023-11-16 19:54:51 +0300

    Update Labex1_Q1_tests_210401072_mehmetemin_tok.R

--- Labex1_Q1_tests_210401072_mehmetemin_tok.R
+++ Labex1_Q1_tests_210401072_mehmetemin_tok.R
@@ -15,31 +15,35 @@ test_that("E??er mevcut ise veri setini silme", {
 current_dir <- getwd()
 relative_path <- file.path(current_dir,"Labex1_Q1_210401072_mehmetemin_tok.R")
 source(relative_path)
-
 test_that("Test : MapsThatChangedOurWorld_StoryMap_Data.csv adl?? dosya belirtilen dizinde mevcuttur.", {
   file_path <- file.path(current_dir,"MapsThatChangedOurWorld_StoryMap_Data.csv")
   expect_true(file.exists(file_path), info = "Dosya mevcut de??il.")
 })
+
 test_that("Test : maps adl?? de??i??ken Global Workspace???de mevcuttur.", {
   expect_true(exists("maps", envir = .GlobalEnv), info = "maps adl?? de??i??ken mevcut de??il.")
-  
 })
+
 test_that("Test : maps nesnesi bir data.frame'dir.", {
   expect_is(maps, "data.frame", info = "maps nesnesi bir data.frame de??il.")
 })
+
 test_that("Test : maps adl?? data.frame'in ilk s??tunu 'City' olmal??d??r.", {
   expect_equal(colnames(maps)[1], "City", info = "??lk s??tun 'City' de??il.")
-  
 })
+
 test_that("Test : maps adl?? data.frame'in 5. s??tununun ad??nda 'Title' kelimesi ge??melidir.", {
   expect_true(grepl("Title", colnames(maps)[5], fixed = TRUE), info = "5. s??tunun ad??nda 'Title' kelimesi ge??miyor.")
 })
+
 test_that("Test : Latitude adl?? s??tun numeric de??erlerden olu??mal??d??r.", {
   expect_is(maps$Latitude, "numeric", info = "Latitude s??tunu numeric de??erlerden olu??muyor.")
 })
+
 test_that("Test : Longitude adl?? s??tun numeric de??erlerden olu??mal??d??r.", {
   expect_is(maps$Longitude, "numeric", info = "Longitude s??tunu numeric de??erlerden olu??muyor.")
-  test_that("Test : idx adl?? de??i??ken Global Workspace???de mevcuttur.", {
-    expect_true(exists("idx", envir = .GlobalEnv), info = "idx adl?? de??i??ken mevcut de??il.")
-  })
+})
+
+test_that("Test : idx adl?? de??i??ken Global Workspace???de mevcuttur.", {
+  expect_true(exists("idx", envir = .GlobalEnv), info = "idx adl?? de??i??ken mevcut de??il.")
 })
