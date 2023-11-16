9bd7a15cd58c0c666de477592b053c2052a2dac0
Mehmet Emin Tok <mhmtmntok@gmail.com>
2023-11-16 19:33:36 +0300

    210401072_1_5

---
+++ Labex1_Q1_210401072_mehmetemin_tok.R
@@ -0,0 +1,11 @@
+url <- "https://raw.githubusercontent.com/scizmeli/Red/master/MapsThatChangedOurWorld_StoryMap_Data.csv"
+file_name <- "MapsThatChangedOurWorld_StoryMap_Data.csv"
+download.file(url, destfile = file_name, mode = "wb")
+
+maps <- read.csv(file_name, header = TRUE,sep = ";")
+maps$Latitude <- gsub("N", "", (maps$Latitude))
+maps$Latitude<-as.numeric(maps$Latitude)
+idx <- which(grepl("W", maps$Longitude))
+maps$Longitude <- gsub("E","",maps$Longitude)
+maps$Longitude <- gsub("W","",maps$Longitude)
+maps$Longitude <- as.character(maps$Longitude)
